6af7b2501b80584dd86f25dc1ffa16054f23d2ee
矢田桂都 <yatakeito@makkubukkuea.local>
2022-10-31 11:55:32 +0900

    s

---
+++ sql/book_data.dump.sql
@@ -0,0 +1,65 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.17
+-- Dumped by pg_dump version 14.4
+
+-- Started on 2022-10-25 14:58:58 JST
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+--
+-- TOC entry 196 (class 1259 OID 16387)
+-- Name: book_user; Type: TABLE; Schema: public; Owner: book_user
+--
+
+CREATE TABLE public.book_user (
+    user_id character varying(255) NOT NULL,
+    passwd character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    user_shi character varying(255),
+    user_mei character varying(255),
+    del boolean
+);
+
+
+ALTER TABLE public.book_user OWNER TO book_user;
+
+--
+-- TOC entry 3224 (class 0 OID 16387)
+-- Dependencies: 196
+-- Data for Name: book_user; Type: TABLE DATA; Schema: public; Owner: book_user
+--
+
+COPY public.book_user (user_id, passwd, email, user_shi, user_mei, del) FROM stdin;
+ka	ka	ka	ka	ka	f
+\.
+
+
+--
+-- TOC entry 3102 (class 2606 OID 16394)
+-- Name: book_user book_user_pkey; Type: CONSTRAINT; Schema: public; Owner: book_user
+--
+
+ALTER TABLE ONLY public.book_user
+    ADD CONSTRAINT book_user_pkey PRIMARY KEY (user_id);
+
+
+-- Completed on 2022-10-25 14:59:00 JST
+
+--
+-- PostgreSQL database dump complete
+--
+
