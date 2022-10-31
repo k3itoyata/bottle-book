05f14feda54b96484a84183bdf5866d86e5ad2d9
矢田桂都 <yatakeito@makkubukkuea.local>
2022-10-31 10:42:22 +0900

    a

--- apps.py
+++ apps.py
@@ -1,10 +1,11 @@
+from utils.session import Session
+import routes_login
+import routes_list
 import bottle
 # 各パスルーティング
 import routes
-#import routes_form
-import routes_list
-import routes_login
-from utils.session import Session
+import routes_form
+
 
 app = routes.app
 app_sess = routes.app_sess
@@ -13,3 +14,5 @@ if __name__ == '__main__':
     # this setting is running for development.
     bottle.run(app=app_sess, host='0.0.0.0',
                port=8888, reloader=True, debug=True)
+else:  # 以下を追加する
+    application = app_sess
